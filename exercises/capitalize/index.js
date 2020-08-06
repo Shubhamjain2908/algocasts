ad88c41056a7678ca8eafa7d1b0fcb1be4ff4f98
Shubham Jain <shubhamjain2908@gmail.com>
2020-08-07 01:22:27 +0530

    capitalize: regex & map

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,9 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+    // return str.split(' ').map(v => v.slice(0, 1).toUpperCase() + v.slice(1)).join(' ');
+    return str.replace(/(^|\s)[a-z]/g, s => s.toUpperCase());
+}
 
 module.exports = capitalize;
