ccd1e87e53ba2d391e6603064f2fb8adb4731b43
Shubham Jain <shubhamjain2908@gmail.com>
2020-08-08 02:09:06 +0530

    merge sort complete

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -38,7 +38,15 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
+    }
 
+    const centre = Math.floor(arr.length / 2);
+    const left = arr.slice(0, centre);
+    const right = arr.slice(centre);
+
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
