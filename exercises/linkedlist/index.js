7b9f9e1e3b789ff0bcfb7a958e82c93722ecfcb2
Shubham Jain <shubhamjain2908@gmail.com>
2020-08-09 01:32:27 +0530

    linkedlist: getAt, removeAt & insertAt

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -85,6 +85,56 @@ class LinkedList {
             this.head = newNode;
         }
     }
+
+    getAt(index) {
+        if (!this.head) {
+            return null;
+        }
+
+        let counter = 0;
+        let currentNode = this.head;
+        while (currentNode) {
+            if (counter === index) {
+                return currentNode;
+            }
+            counter++;
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    removeAt(index) {
+        if (!this.head) {
+            return null;
+        }
+
+        if (index === 0) {
+            this.head = this.head.next;
+            return;
+        }
+
+        const previous = this.getAt(index - 1);
+        if (!previous || !previous.next) {
+            return;
+        }
+        previous.next = previous.next.next;
+    }
+
+    insertAt(data, index) {
+        if (!this.head) {
+            this.head = new Node(data);
+            return;
+        }
+
+        if (index === 0) {
+            this.head = new Node(data, this.head);
+            return;
+        }
+
+        const previous = this.getAt(index - 1) || this.getLast();
+        const newNode = new Node(data, previous.next);
+        previous.next = newNode;
+    }
 }
 
 module.exports = { Node, LinkedList };
